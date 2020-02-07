62f8d6030710cc29e3d2c740a1461921b5ef84ea
michaeldidia <mikegoescoding@gmail.com>
2020-02-07 12:09:14 -0700

    Added commentary notes

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 print('Hi Git')
+
+# steps --------
+#> git init (only once per project for a new project)
+##> git status (optional, just to see status of tracked/untracked and left to be committed)
+#> git add .('dot') - (for all files) -OR- git add my_filename.py (for just a single file)
+##> git status (optional, just to see status of tracked/untracked and left to be committed)
+#> git commit -m "Name my commit here" (make sure to to leave a message with -m!!!!)
+##> git status (optional, just to see status of tracked/untracked and left to be committed)
+
+#> git add . (for mwhen you make changes)
+#> git commit -m "Changes I made to the file"
